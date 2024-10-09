ddd5f76dd1b5afa2afdf2066e5361f5bc7474356
arjunss25 <arjunsuresh386@gmail.com>
2024-10-09 16:04:56 +0530

    updates

--- app/Components/Testimonial.jsx
+++ app/Components/Testimonial.jsx
@@ -4,7 +4,7 @@ const Testimonial = ({data}) => {
     <>
         {data.map((item,index)=>{
             return(
-                <div className='w-[30vw] h-[35vh] bg-white rounded-[2rem] border-[2px] border-black'>
+                <div key={index} className='w-[30vw] h-[35vh] bg-white rounded-[2rem] border-[2px] border-black'>
                     <div className="top-sec w-full h-[50%]  flex items-center justify-between px-10 ">
                         <div className="profile-img w-20 h-20 rounded-full bg-black">
                             <img src={item.profile} alt="" />
--- app/products/page.js
+++ app/products/page.js
@@ -2,6 +2,7 @@
 import Heroslider from '../Components/Heroslider'
 import MainNavbar from '../Components/MainNavbar'
 import Secondarynavbar from '../Components/Secondarynavbar'
+import { CiFilter } from "react-icons/ci";
 
 const page = () => {
 
@@ -10,6 +11,7 @@ const page = () => {
         { image: "/hero1.jpg", alt: "Hero image 2 description" },
         { image: "/hero1.jpg", alt: "Hero image 3 description" },
       ];
+      
 
 
 
@@ -18,17 +20,21 @@ const page = () => {
     <div>
         <MainNavbar/>
         <Secondarynavbar/>
-        <div className='content-sec flex'>
+        <div className='content-sec flex relative'>
 
-            <div className='sidebar-sec w-[20vw] h-screen bg-red-500'>
+            <div className='sidebar-sec w-[300px]  h-screen bg-red-500 absolute md:relative z-[50] hidden lg:block '>
 
             </div>
 
-            <div className='main-sec w-[80vw] h-screen bg-blue-500 px-10 py-10'>
+            <div className='main-sec w-full lg:w-[calc(100%-300px)] min-h-screen  px-10 py-2 lg:py-10'>
+                <div className='filter-nav w-full h-[10vh] visible lg:hidden'>
+                    <CiFilter className='text-[2rem]' />
+                </div>
                 <Heroslider images={images}/>
             </div>
 
         </div>
+
     </div>
   )
 }
