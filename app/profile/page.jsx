14a725c645ecb33056dcfe28d1477ecbd60a2464
arjunss25 <arjunsuresh386@gmail.com>
2024-10-17 12:54:07 +0530

    updates

---
+++ app/profile/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainNavbar from '../Components/MainNavbar'
+import Secondarynavbar from '../Components/Secondarynavbar'
+
+const Profile = () => {
+  return (
+    <div>
+      <MainNavbar/>
+      <Secondarynavbar/>
+    </div>
+  )
+}
+
+export default Profile
