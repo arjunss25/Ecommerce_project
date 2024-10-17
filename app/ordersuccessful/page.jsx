b71d776770d4ff8181c1b95184f4996ff0c29cc5
arjunss25 <arjunsuresh386@gmail.com>
2024-10-17 09:51:12 +0530

    updates

--- app/ordersuccessful/page.jsx
+++ app/ordersuccessful/page.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MainNavbar from '../Components/MainNavbar'
 import Secondarynavbar from '../Components/Secondarynavbar'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const page = () => {
   return (
@@ -13,6 +14,17 @@ const page = () => {
         <Image src={'/paymentsuccessful.png'} width={120} height={120}></Image>
         <h1 className='mt-10 text-[3rem] font-medium text-center'>Thank You ! <span className='bg-[#D7F400] p-2'>Order Successful</span></h1>
       </div>
+
+
+
+      {/* sec-btn-bar */}
+      <div className="sec-btn-nav w-full h-[12vh] bg-red-500 flex items-center justify-between border-t-[2px] border-b-[2px] border-black px-10">
+        <div className="left-btns flex gap-5">
+          <Link href="" className='px-4 py-1 rounded-full border-[1px] border-black'>Login</Link>
+          <Link href="" className='px-4 py-1 rounded-full border-[1px] border-black'>Signup</Link>
+        </div>
+      </div>
+
     </div>
   )
 }
