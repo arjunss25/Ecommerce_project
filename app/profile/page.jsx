9f101a7cb058811432a3d9a1884150aeb0ca0341
arjunss25 <arjunsuresh386@gmail.com>
2024-10-17 13:47:58 +0530

    updates

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,6 +7,16 @@ const Profile = () => {
     <div>
       <MainNavbar/>
       <Secondarynavbar/>
+
+      <div className="content p-10 flex">
+        <div className="left-side w-[30vw] h-screen bg-red-100">
+
+        </div>
+
+        <div className="right-side">
+
+        </div>
+      </div>
     </div>
   )
 }
